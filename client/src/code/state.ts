240fb17e9c5ba2a290c27d0c074036733f0601ec
Michael Haile <Michael.Haile@TechSur.Solutions>
2021-12-28 08:10:37 -0500

    Fix Typo

--- client/src/code/state.ts
+++ client/src/code/state.ts
@@ -6,7 +6,7 @@ export function createState(inParentComponent) {
   return {
     
         //States
-        pleaseWaitVisisble:false,
+        pleaseWaitVisible:false,
         contacts:[],
         mailboxes:[],
         messages:[],
